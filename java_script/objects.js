99844b64030a78802918b260e63e6684a97592db
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-03-11 00:19:57 +0530

    js_code

---
+++ java_script/objects.js
@@ -0,0 +1,18 @@
+let product={
+company : 'Tata',
+'net-worth' : '155 Billions',
+since : 1868,
+};
+console.log(product);
+console.log(product.company);
+console.log(product.since);
+// console.log(product.net-worth); // this will throw an error
+console.log(product['net-worth'])
+
+
+console.log('Types of a Object ');
+console.log(typeof product);
+console.log(typeof product.since);
+
+
+
