7809404b90191720745adb15a936aefd3c86b605
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-05-18 13:37:41 +0530

    changes are commited

---
+++ java_script/03java.js
@@ -0,0 +1,17 @@
+// MATH OBJECTS 
+
+// MATH FLOOR
+let result=Math.floor(3.9999999999); // 3
+console.log(result);
+
+// MATH SQRT
+console.log(Math.sqrt(6785));
+
+// MATH CEIL 
+console.log(Math.ceil(3.01)); // 4
+
+// MATH ROUND 
+console.log(Math.round(3.49));  // 3
+
+Math.floor(Math.random()*6);
+
