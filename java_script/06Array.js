20bf996c666f432896fac07c3b91e573308f9996
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-05-24 00:00:07 +0530

    simple way

--- java_script/06Array.js
+++ java_script/06Array.js
@@ -1,1 +1,10 @@
 // THERE ARE DIFFERENT WAYS OF CREATING ARRAY IN JAVASCRIPT 
+
+
+// Simplest way of creating the array 
+let arr=[10,20,30,40,50];
+
+for(let i=0;i<arr.length;i++)
+  {
+    console.log(arr[i]);
+  }
