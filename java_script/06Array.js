5912bb55ffe40027bed3d4c666166b4c5def873a
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-06-09 15:21:00 +0530

    updated_js

--- java_script/06Array.js
+++ java_script/06Array.js
@@ -49,18 +49,68 @@ for(let i=0;i<arr.length;i++)
       //  2.pop()
       //  3.shift()   ---> Removes the element from first Index
       //  4.unshift() ----> Add the element from the First Index
-      //  5.concat()
-      //  6.indexOf()
-      // 
+      //  5.concat()  ----> conacte the two array 
+      //  6.indexOf()  ----> Give the index of the element
+      //  7.Includes()   ---> Gives True or false value that the given value present in the array or not 
+      //  8.reverse()
+      //  9.sort()
+      //  10.slice()
+      //  11.splice()
 
 
       console.log('\n\n');
       newarr=[0,2,4,6];
+      newar=[1,3,5,7];
       newarr.shift();
 
       for(let i=0;i<newarr.length;i++)
         {
           console.log(newarr[i]);
         }
+
+        newarr.unshift(0);
+
+        
+      for(let i=0;i<newarr.length;i++)
+        {
+          console.log(newarr[i]);
+        }
+
+
+        console.log(newarr.includes(6));
+        console.log(newarr.includes(7));
+
+        console.log(newarr.indexOf(6));
+        console.log(newarr.indexOf(7));
+
+
+        const concate_array=newarr.concat(newar);
+        for(let i=0;i<concate_array.length;i++)
+          {
+            console.log(concate_array[i]);
+          }
+
+          console.log('Reverse an array\n');
+          concate_array.reverse();
+          for(let i=0;i<concate_array.length;i++)
+            {
+              console.log(concate_array[i])
+            }
+
+            console.log('sort an array ');
+            concate_array.sort();
+             for(let i=0;i<concate_array.length;i++)
+              {
+                console.log(concate_array[i]);
+              }
+
+
+
+
+
+
+
+
+
 
 
