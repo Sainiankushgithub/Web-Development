247a4d33e31adfa7650affed375b29a5ee7940a4
SainiAnkushgithub <www.ankushsaini55@gmail.com>
2024-09-28 00:00:15 +0530

    string

---
+++ java_script/basic_01/04_string.js
@@ -0,0 +1,8 @@
+let gameName="clashOfClan";
+
+console.log(gameName);
+console.log(typeof(gameName));
+
+let gameName1 = new String('ankush');
+console.log(gameName1);
+console.log(typeof(gameName1));
